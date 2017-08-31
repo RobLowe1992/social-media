0353ab8cec45ebc5cd0ff5303a0949bdc2a2178a
Robert Lowe <RobLowe1992@Yahoo.com>
2017-08-31 00:02:23 -0400

    Working on dynamic post index

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :image, styles: {large:"600x600", medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: {large:"600x600", medium: "300x300>", thumb: "100x100>", icon:"50x50" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   has_many :active_friendships, class_name: "Friendship", foreign_key: "follower_id", dependent: :destroy
